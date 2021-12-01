b260b2f1fccc247209141a408c29d129633f61b8
sparks333 <76711232+sparks333@users.noreply.github.com>
2021-12-01 02:11:27 -0600

    Create Day1.py

---
+++ Day1/Day1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+input = np.loadtxt('input.txt')
+
+greater = input[1:] > input[0:-1]
+
+print("Part 1:")
+print(greater.sum())
+
+conv = np.convolve(input, np.array([1, 1, 1]), 'valid')
+
+greater = conv[1:] > conv[0:-1]
+
+print("Part 2:")
+print(greater.sum())
