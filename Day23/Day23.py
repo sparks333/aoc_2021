e22e479b21e3b5ad60a500913b35c5e0943d9d7d
Dane Bennington <sparks333@gmail.com>
2021-12-25 00:16:16 -0800

    Day 23 Final

--- Day23/Day23.py
+++ Day23/Day23.py
@@ -4,6 +4,8 @@ import copy
 from itertools import count
 from collections import defaultdict
 
+from numpy.typing import _128Bit
+
 def is_complete(game_map):
     if game_map[3, 3] == 1 and game_map[2, 3] == 1 and \
        game_map[3, 5] == 2 and game_map[2, 5] == 2 and \
@@ -143,31 +145,31 @@ def valid_move_pt2(y_start, x_start, y_end, x_end, game_map):
         return False
     
     # Failure to move all the way to the back of the room
-    if y_end > 1 and np.all(game_map[y_end:7, x_end] == 0):
+    if y_end > 1 and y_end < 5 and np.all(game_map[y_end+1:6, x_end] == 0):
         return False
 
     # Placing an amphipod in a room with a wrong amphipod behind it
-    if y_end > 1 and np.any(game_map[y_end:6, x_end] != game_map[y_start, x_start]):
+    if y_end > 1 and y_end < 5 and np.any(game_map[y_end+1:6, x_end] != game_map[y_start, x_start]):
         return False
 
     # Moving finalized pieces (A)
     if game_map[y_start, x_start] == 1 and x_start == 3:
-        if y_start > 2 and np.all(game_map[y_start:6, x_start] == 1):
+        if y_start > 1 and np.all(game_map[y_start:6, x_start] == 1):
             return False
 
     # Moving finalized pieces (B)
     if game_map[y_start, x_start] == 2 and x_start == 5:
-        if y_start > 2 and np.all(game_map[y_start:6, x_start] == 2):
+        if y_start > 1 and np.all(game_map[y_start:6, x_start] == 2):
             return False
 
     # Moving finalized pieces (C)
     if game_map[y_start, x_start] == 3 and x_start == 7:
-        if y_start > 2 and np.all(game_map[y_start:6, x_start] == 3):
+        if y_start > 1 and np.all(game_map[y_start:6, x_start] == 3):
             return False
 
     # Moving finalized pieces (D)
     if game_map[y_start, x_start] == 4 and x_start == 9:
-        if y_start > 2 and np.all(game_map[y_start:6, x_start] == 4):
+        if y_start > 1 and np.all(game_map[y_start:6, x_start] == 4):
             return False
 
     # Blocked room
@@ -192,55 +194,65 @@ def move(y_start, x_start, y_end, x_end, game_map):
     gm[y_end, x_end] = game_map[y_start, x_start]
     return ((10**(game_map[y_start, x_start]-1)) * (abs(x_end - x_start) + abs(y_start - 1) + abs(y_end - 1)), gm)
 
-#game_map = -1*np.ones([5, 13])
+game_map = -1*np.ones([5, 13])
 
-#with open('input.txt') as file:
-#    y = 0
-#    for line in file:
-#        x=0
-#        for ch in line[:-1]:
-#            if ch == '.':
-#                game_map[y,x] = 0
-#            elif ch == 'A':
-#                game_map[y, x] = 1
-#            elif ch == 'B':
-#                game_map[y,x] = 2
-#            elif ch == 'C':
-#                game_map[y,x] = 3
-#            elif ch == 'D':
-#                game_map[y,x] = 4
-#            x+=1
-#        y+=1
+with open('input.txt') as file:
+    y = 0
+    for line in file:
+        x=0
+        for ch in line[:-1]:
+            if ch == '.':
+                game_map[y,x] = 0
+            elif ch == 'A':
+                game_map[y, x] = 1
+            elif ch == 'B':
+                game_map[y,x] = 2
+            elif ch == 'C':
+                game_map[y,x] = 3
+            elif ch == 'D':
+                game_map[y,x] = 4
+            x+=1
+        y+=1
     
-#counter = count()
-#frontier = [[0, next(counter), game_map]]
-#explored = {}
-#costs = defaultdict(lambda: float('inf'))
+counter = count()
+frontier = [[0, next(counter), game_map]]
+explored = {}
+costs = defaultdict(lambda: float('inf'))
 
-#while True:
+while True:
 
-#    node = heapq.heappop(frontier)
+    node = heapq.heappop(frontier)
 
-#    explored[tuple(map(tuple, node[2]))] = node
+    explored[tuple(map(tuple, node[2]))] = node
 
-#    if is_complete(node[2]):
-#        print('Part 1: ' + str(node[0]))
-#        break
-#    for y_start in range(1, 4):
-#        for x_start in range(1,12):
-#            for y_end in range(1, 4):
-#                for x_end in range(1,12):
-#                    if valid_move(y_start, x_start, x_end, y_end, node[2]):
-#                        attempt = move(y_start, x_start, x_end, y_end, node[2])
-#                        new_cost = node[0] + attempt[0]
-#                        new_map = attempt[1]
-#                        new_key = tuple(map(tuple, new_map))
-#                        if new_key in explored:
-#                            continue
-#                        if costs[new_key] > new_cost:
-#                            costs[new_key] = new_cost
-#                            heapq.heappush(frontier, [new_cost, next(counter), new_map])
+    if is_complete(node[2]):
+        print('Part 1: ' + str(node[0]))
+        break
+    for y_start in range(1, 4):
+        for x_start in range(1,12):
+            for y_end in range(1, 4):
+                for x_end in range(1,12):
+                    if valid_move(y_start, x_start, x_end, y_end, node[2]):
+                        attempt = move(y_start, x_start, x_end, y_end, node[2])
+                        new_cost = node[0] + attempt[0]
+                        new_map = attempt[1]
+                        new_key = tuple(map(tuple, new_map))
+                        if new_key in explored:
+                            continue
+                        if costs[new_key] > new_cost:
+                            costs[new_key] = new_cost
+                            heapq.heappush(frontier, [new_cost, next(counter), new_map])
 
+def est_cost(game_map):
+    total_cost = 0
+    for y in range(1, 6):
+        for x in range(1,12):
+            a_type = game_map[y,x]
+            if game_map[y,x] > 0:
+                target_x = 2*a_type+1
+                if x != target_x:
+                    total_cost += (10**(a_type-1)) * (abs(x-target_x) + y)
+    return total_cost
 
 game_map = -1*np.ones([7, 13])
 
@@ -271,42 +283,46 @@ while True:
 
     node = heapq.heappop(frontier)
 
-    for y in range(7):
-        for x in range(13):
-            if node[2][y,x] == -1:
-                chout = '#'
-            elif node[2][y,x] == 0:
-                chout = '.'
-            elif node[2][y,x] == 1:
-                chout = 'A'
-            elif node[2][y,x] == 2:
-                chout = 'B' 
-            elif node[2][y,x] == 3:
-                chout = 'C'
-            elif node[2][y,x] == 4:
-                chout = 'D'
-            print(chout, end='')
-        print()
-    print('Cost: ' + str(node[0]))
-    print()
+    node_cost = node[0]
+    node_map = node[2]
 
-    explored[tuple(map(tuple, node[2]))] = node
+    #for y in range(7):
+    #    for x in range(13):
+    #        if node_map[y,x] == -1:
+    #            chout = '#'
+    #        elif node_map[y,x] == 0:
+    #            chout = '.'
+    #        elif node_map[y,x] == 1:
+    #            chout = 'A'
+    #        elif node_map[y,x] == 2:
+    #            chout = 'B' 
+    #        elif node_map[y,x] == 3:
+    #            chout = 'C'
+    #        elif node_map[y,x] == 4:
+    #            chout = 'D'
+    #        print(chout, end='')
+    #    print()
+    #print('Cost: ' + str(node[0]))
+    #print()
 
-    if is_complete_pt2(node[2]):
-        print('Part 1: ' + str(node[0]))
+    explored[tuple(map(tuple, node_map))] = node
+
+    if is_complete_pt2(node_map):
+        print('Part 2: ' + str(node_cost))
         break
     for y_start in range(1, 6):
         for x_start in range(1,12):
             for y_end in range(1, 6):
                 for x_end in range(1,12):
-                    if valid_move_pt2(y_start, x_start, y_end, x_end, node[2]):
-                        attempt = move(y_start, x_start, y_end, x_end, node[2])
-                        new_cost = node[0] + attempt[0]
+                    if valid_move_pt2(y_start, x_start, y_end, x_end, node_map):
+                        attempt = move(y_start, x_start, y_end, x_end, node_map)
+                        new_cost = node_cost + attempt[0]
                         new_map = attempt[1]
                         new_key = tuple(map(tuple, new_map))
                         if new_key in explored:
                             continue
                         if costs[new_key] > new_cost:
+                            e_cost = est_cost(new_map)
                             costs[new_key] = new_cost
                             heapq.heappush(frontier, [new_cost, next(counter), new_map])
 
